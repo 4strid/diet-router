26d8f133dcb562b4d3a95140aa6564591919a8c5
Peter Fesz-Nguyen <pfesznguyen@gmail.com>
2017-11-03 14:37:30 -0700

    working order

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
-function Router (path, ...middleware) {
-	var fn = function route (app, path, middleware) {
-		var middleware = [...this.middleware, ...middleware]
-		var fullPath = this.path + path
-		var args = [fullPath, ...middleware]
-		o.queue.forEach(function (op) {
-			app[op.method].apply(app, op.args)
-		})
+function Router (...middleware) {
+	var fn = function route (app, path, ...middleware) {
+		fn.app = app
+		fn.path = path
+		fn.middleware = [...fn.middleware, ...middleware]
+		for (const op of fn.queue) {
+			method.call(fn, op.method, op.path, op.middleware)
+		}
 	}
 	fn.queue = []
 
-	fn.path = path
+	fn.app = null
 	fn.middleware = middleware
+	fn.route = function (path, ...middleware) {
+		var router = middleware.pop()
+		router(this, path, middleware)
+	}
 
 	return new Proxy(fn, {
 		get: function (target, name) {
+			if (target.hasOwnProperty(name)) {
+				return target[name]
+			}
 			return function(path, ...middleware) {
-				this.queue.push({
-					method: name,
-					path,
-					middleware
-				})
+				method.call(this, name, path, middleware)
 			}
 		}
 	})
@@ -29,6 +32,21 @@ Router.extend = function (app) {
 	app.route = function (path, ...middleware) {
 		var router = middleware.pop()
 		router(app, path, ...middleware)
+	}
+}
+
+function method (method, path, middleware) {
+	if (this.app === null) {
+		this.queue.push({
+			method,
+			path,
+			middleware
+		})
+	} else {
+		var nestedMiddleware = [...this.middleware, ...middleware]
+		var nestedPath = this.path + path
+		var args = [nestedPath, ...nestedMiddleware]
+		this.app[method].apply(this.app, args)
 	}
 }
 
--- test/test.js
+++ test/test.js
@@ -1,6 +1,183 @@
 var test = require('tape')
 var server = require('diet')
+var request = require('request-promise-native')
 var Router = require('../')
+
+/*********
+ * Setup *
+ *********/
 
 var app = server()
 app.listen('http://localhost:7777')
+Router.extend(app)
+
+function fooware ($) {
+	$.foo = 'foo'
+	$.return
+}
+
+function barware ($) {
+	$.bar = 'bar'
+	$.return
+}
+
+function bazware ($) {
+	$.baz = 'baz'
+	$.return
+}
+
+/*********
+ * Tests *
+ *********/
+
+test('Expect router to add an accessible route when the router is called directly and when methods are called before the router', function (t) {
+	var router = Router()
+	router.get('/', function ($) {
+		$.end('test success')
+	})
+	router(app, '/test1')
+
+	request('http://localhost:7777/test1/')
+	.then(function (res) {
+		t.equal(res, 'test success', 'got the expected response')
+		t.end()
+	})
+	.catch(function (err) {
+		t.fail('this test should not throw an error: ' + err.message)
+		t.end()
+	})
+})
+
+
+test('Expect router to add an accessible route when the router is called directly and when the router is called before methods', function (t) {
+	var router = Router()
+	router(app, '/test2')
+	router.get('/', function ($) {
+		$.end('test success')
+	})
+
+	request('http://localhost:7777/test2/')
+	.then(function (body) {
+		t.equal(body, 'test success', 'got the expected response')
+		t.end()
+	})
+	.catch(function (err) {
+		t.fail('this test should not throw an error: ' + err.message)
+		t.end()
+	})
+})
+
+test('Expect router to add an accessible route when the router is invoked by "route" and when methods are called before "route"', function (t) {
+	var router = Router()
+	router.get('/', function ($) {
+		$.end('test success')
+	})
+	app.route('/test3', router)
+
+	request('http://localhost:7777/test3/')
+	.then(function (body) {
+		t.equal(body, 'test success', 'got the expected response')
+		t.end()
+	})
+	.catch(function (err) {
+		t.fail('this test should not throw an error: ' + err.message)
+		t.end()
+	})
+})
+
+test('Expect router to add an accessible route when the router is invoked by "route" and when the router is called before methods', function (t) {
+	var router = Router()
+	app.route('/test4', router)
+	router.get('/', function ($) {
+		$.end('test success')
+	})
+
+	request('http://localhost:7777/test4/')
+	.then(function (body) {
+		t.equal(body, 'test success', 'got the expected response')
+		t.end()
+	})
+	.catch(function (err) {
+		t.fail('this test should not throw an error: ' + err.message)
+		t.end()
+	})
+})
+
+test('Expect router to add routes when methods are added before and after calling the router', function (t) {
+	var router = Router()
+	router.get('/1', function ($) {
+		$.end('test success 1')
+	})
+	router(app, '/test5')
+	router.get('/2', function ($) {
+		$.end('test success 2')
+	})
+
+	request('http://localhost:7777/test5/1')
+	.then(function (body) {
+		t.equal(body, 'test success 1', 'got the expected response')
+		return request('http://localhost:7777/test5/2')
+	})
+	.then(function (body) {
+		t.equal(body, 'test success 2', 'got the expected response')
+		t.end()
+	})
+	.catch(function (err) {
+		t.fail('this test should not throw an error: ' + err.message)
+		t.end()
+	})
+})
+
+test('Expect router to add routes when methods are added before and after calling "route"', function (t) {
+	var router = Router()
+	router.get('/1', function ($) {
+		$.end('test success 1')
+	})
+	app.route('/test6', router)
+	router.get('/2', function ($) {
+		$.end('test success 2')
+	})
+
+	request('http://localhost:7777/test6/1')
+	.then(function (body) {
+		t.equal(body, 'test success 1', 'got the expected response')
+		return request('http://localhost:7777/test6/2')
+	})
+	.then(function (body) {
+		t.equal(body, 'test success 2', 'got the expected response')
+		t.end()
+	})
+	.catch(function (err) {
+		t.fail('this test should not throw an error: ' + err.message)
+		t.end()
+	})
+})
+
+test('Expect router to add routes when methods are added before and after calling "route"', function (t) {
+	var router = Router()
+	router.get('/1', function ($) {
+		$.end('test success 1')
+	})
+	app.route('/test6', router)
+	router.get('/2', function ($) {
+		$.end('test success 2')
+	})
+
+	request('http://localhost:7777/test6/1')
+	.then(function (body) {
+		t.equal(body, 'test success 1', 'got the expected response')
+		return request('http://localhost:7777/test6/2')
+	})
+	.then(function (body) {
+		t.equal(body, 'test success 2', 'got the expected response')
+		t.end()
+	})
+	.catch(function (err) {
+		t.fail('this test should not throw an error: ' + err.message)
+		t.end()
+	})
+})
+
+test.onFinish(function () {
+	process.exit(0)
+})
