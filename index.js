f6133855e0ec0cc3ff54880dc5eb5f58ac61ff7d
Peter Fesz-Nguyen <pfesznguyen@gmail.com>
2017-11-03 00:16:04 +0000

    Refactor

--- index.js
+++ index.js
@@ -1,63 +1,35 @@
-function Router (path) {
-	var fn = function route (app) {
+function Router (path, ...middleware) {
+	var fn = function route (app, path, middleware) {
+		var middleware = [...this.middleware, ...middleware]
+		var fullPath = this.path + path
+		var args = [fullPath, ...middleware]
 		o.queue.forEach(function (op) {
 			app[op.method].apply(app, op.args)
 		})
 	}
 	fn.queue = []
 
+	fn.path = path
+	fn.middleware = middleware
+
 	return new Proxy(fn, {
 		get: function (target, name) {
-			var middleware = target.middleware.concat(Array.apply(null, arguments).slice(1))
-			var fullPath = target.path + path
-			var args = [fullPath].concat(middleware)
-			target.queue.push({
-				method: method,
-				args: args
-			})
+			return function(path, ...middleware) {
+				this.queue.push({
+					method: name,
+					path,
+					middleware
+				})
+			}
 		}
 	})
-	if (app === undefined && path === undefined) {
-		return DeferredRouter()
-	}
-	o.path = path
-	o.middleware = Array.apply(null, arguments).slice(1)
-	return o
 }
-
-function DeferredRouter () {
-	var o = Object.create(DeferredRouter.prototype)
-	o.queue = []
-	return o
-}
-
-var methodNames = ['get', 'post', 'put', 'patch', 'head', 'delete', 'trace']
-var methods = methodNames.map(function (name) {
-	return function (path) {
-		var middleware = this.middleware.concat(Array.apply(null, arguments).slice(1))
-		var fullPath = this.path + path
-		var args = [fullPath].concat(middleware)
-		this.app[method].apply(null, args)
-	}
-})
-
-methods.forEach(function (method) {
-	Router.prototype[method] = function (path) {
-		var middleware = this.middleware.concat(Array.apply(null, arguments).slice(1))
-		var fullPath = this.path + path
-		var args = [fullPath].concat(middleware)
-		this.app[method].apply(null, args)
-	}
-
-	DeferredRouter.prototype[method] = function (path) {
-	}
-})
 
 Router.extend = function (app) {
-	app.route
+	app.route = function (path, ...middleware) {
+		var router = middleware.pop()
+		router(app, path, ...middleware)
+	}
 }
 
 module.exports = Router
-
-// add 'route' method to app
-// which just wraps Router
